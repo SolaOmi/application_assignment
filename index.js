1300d5629e4b6378510bcc6563a5b58dc81c8a49
sola <solaocodes@gmail.com>
2019-05-14 20:09:28 -0400

    Install and require random-movie-quotes package (bug fix).

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const app = express();
-const port = 3000;
+const quotes  = require('random-movie-quotes');
+const app     = express();
+const port    = 3000;
 
 app.use(express.static('public'));
 
